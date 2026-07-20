3c325078172279154bf6034582c6d9034b738951
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2026-07-20 14:40:34 +0200

    Application name changed as well as UI controller moved to a dedicated directory.

--- src/main/java/com/plociennik/vestal/VestalApplication.java
+++ src/main/java/com/plociennik/vestal/VestalApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class VestalApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(VestalApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Vestal");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/plociennik/vestal/ui/MainController.java
+++ src/main/java/com/plociennik/vestal/ui/MainController.java
@@ -1,14 +1,14 @@
-package com.plociennik.vestal;
+package com.plociennik.vestal.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to Vestal!");
     }
 }
