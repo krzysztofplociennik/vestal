e534d7bad4bf212a1d2a55442ae9eba47f7afb11
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2026-07-22 14:19:40 +0200

    Added a custom exception.

---
+++ src/main/java/com/plociennik/vestal/common/VestalException.java
@@ -0,0 +1,11 @@
+package com.plociennik.vestal.common;
+
+public class VestalException extends RuntimeException {
+    public VestalException(String errorID, String message) {
+        super("(%s) %s".formatted(errorID, message));
+    }
+
+    public VestalException(String errorID, String message, Exception e) {
+        super("(%s) %s\nCause: [%s]".formatted(errorID, message, e.toString()));
+    }
+}
