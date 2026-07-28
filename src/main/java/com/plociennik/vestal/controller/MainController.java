ca09ec29650ee62d22674060bc7dcea550e1edf0
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2026-07-28 20:19:08 +0200

    Deleted unneeded code from the main controller.

--- src/main/java/com/plociennik/vestal/controller/MainController.java
+++ src/main/java/com/plociennik/vestal/controller/MainController.java
@@ -1,15 +1,4 @@
 package com.plociennik.vestal.controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class MainController {
-
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Vestal!");
-    }
 }
