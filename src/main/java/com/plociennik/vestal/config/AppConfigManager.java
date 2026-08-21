c2fcb921af587452f5f4cb8d59b07e247a443149
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2026-08-21 16:36:25 +0200

    Deleted unused methods from AppConfigManager.

--- src/main/java/com/plociennik/vestal/config/AppConfigManager.java
+++ src/main/java/com/plociennik/vestal/config/AppConfigManager.java
@@ -30,21 +30,6 @@ public class AppConfigManager {
         return mapper.readValue(configFile, AppConfig.class);
     }
 
-//    // todo: to delete, saveConfig should handle
-//    public void saveDirectoryPath(String path) {
-//        loadCurrentConfig();
-//        currentConfig.vestalRepository.localRepository.sourcePath = path;
-//        mapper.writeValue(configFile.toPath().toFile(), currentConfig);
-//    }
-
-//    // todo: to delete, saveConfig should handle
-//    public void saveRepositoryNameAndUrl(String name, String url) {
-//        loadCurrentConfig();
-//        currentConfig.vestalRepository.remoteRepository.name = name;
-//        currentConfig.vestalRepository.remoteRepository.url = url;
-//        mapper.writeValue(configFile.toPath().toFile(), currentConfig);
-//    }
-
     public void saveConfig(AppConfig updatedConfig) {
         mapper.writeValue(configFile.toPath().toFile(), updatedConfig);
         loadCurrentConfig();
